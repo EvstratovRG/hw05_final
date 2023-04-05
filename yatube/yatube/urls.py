c9ce8cf8ea40afe8df1e3edb5fd76db76fb0001c
= <=>
2023-04-05 10:42:26 +0300

    closed debug toolbar

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-import debug_toolbar
 
 
 urlpatterns = [
@@ -22,4 +21,5 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+    # import debug_toolbar
+    # urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
